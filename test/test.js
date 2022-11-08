fa4edab29d8deb6a7fc1989fbe33c446d7eb8db5
Daniel_Stoxreiter <daniel.stoxreiter@outlook.de>
2022-11-08 14:36:41 +0100

    simplified code example

--- test/test.js
+++ test/test.js
@@ -10,11 +10,9 @@ var editor = new LoadEditor({
                 color: "color-feature1",
                 html_class: "html-class-feature1",
                 css_class: "css-class-feature1",
-                hide: false,
-                chg_citation: "citation-url",
                 features: {
                     all: false,
-                    class: "single-feature"
+                    class: "all-features"
                 }
             },
             {
@@ -25,32 +23,22 @@ var editor = new LoadEditor({
                 html_class: "html-class-feature2",
                 css_class: "css-class-feature2",
                 hide: true,
-                chg_citation: "citation-url",
                 features: {
                     all: false,
-                    class: "single-feature"
+                    class: "all-features"
                 }
             },
             {
                 opt: "feature3",
                 opt_slider: "feature3-slider",
                 title: "All Features",
-                hide: false,
-                chg_citation: "citation-url",
+                color: "color-feature3",
                 features: {
                     all: true,
                     class: "all-features"
                 }
             }
-        ],
-        span_element: {
-            css_class: "badge-item"
-        },
-        active_class: "activated",
-        rendered_element: {
-            label_class: "switch",
-            slider_class: "i-slider round"
-        }
+        ]
     },
     ff: {
         name: "Change font family",
@@ -58,8 +46,6 @@ var editor = new LoadEditor({
             {
                 opt: "select-font",
                 title: "Font family",
-                urlparam: "font",
-                chg_citation: "citation-url",
                 fonts: {
                     default: "default",
                     font1: "Times-New-Roman",
@@ -68,12 +54,9 @@ var editor = new LoadEditor({
                     font4: "Verdana-serif"
                 },
                 paragraph: "p",
-                p_class: "yes-index",
-                css_class: ""
+                p_class: "yes-index"
             }
-        ],
-        active_class: "active",
-        html_class: "custom-select"
+        ]
     },
     fs: {
         name: "Create full size mode",
@@ -81,47 +64,29 @@ var editor = new LoadEditor({
             {
                 opt: "edition-fullsize1",
                 title: "Full screen on/off",
-                urlparam: "fullscreen",
-                chg_citation: "citation-url",
                 hide: "hide-container1",
-                to_hide: "fade"
             },
             {
                 opt: "edition-fullsize2",
                 title: "Full screen on/off",
-                urlparam: "fullscreen",
-                chg_citation: "citation-url",
                 hide: "hide-container2",
-                to_hide: "fade"
             },
             {
                 opt: "edition-fullsize3",
                 title: "Full screen on/off",
-                urlparam: "fullscreen",
-                chg_citation: "citation-url",
                 hide: "hide-container3",
-                to_hide: "fade"
             },
             {
                 opt: "edition-fullsize4",
                 title: "Full screen on/off",
-                urlparam: "fullscreen",
-                chg_citation: "citation-url",
                 hide: "hide-container4",
-                to_hide: "fade"
             },
             {
                 opt: "edition-fullsize5",
                 title: "Full screen on/off",
-                urlparam: "fullscreen",
-                chg_citation: "citation-url",
                 hide: "hide-container5",
-                to_hide: "fade"
             }
-        ],
-        active_class: "active",
-        render_class: "nav-link btn btn-round",
-        render_svg: "<svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-fullscreen' viewBox='0 0 16 16'><path d='M1.5 1a.5.5 0 0 0-.5.5v4a.5.5 0 0 1-1 0v-4A1.5 1.5 0 0 1 1.5 0h4a.5.5 0 0 1 0 1h-4zM10 .5a.5.5 0 0 1 .5-.5h4A1.5 1.5 0 0 1 16 1.5v4a.5.5 0 0 1-1 0v-4a.5.5 0 0 0-.5-.5h-4a.5.5 0 0 1-.5-.5zM.5 10a.5.5 0 0 1 .5.5v4a.5.5 0 0 0 .5.5h4a.5.5 0 0 1 0 1h-4A1.5 1.5 0 0 1 0 14.5v-4a.5.5 0 0 1 .5-.5zm15 0a.5.5 0 0 1 .5.5v4a1.5 1.5 0 0 1-1.5 1.5h-4a.5.5 0 0 1 0-1h4a.5.5 0 0 0 .5-.5v-4a.5.5 0 0 1 .5-.5z'/></svg>"
+        ]
     },
     fos: {
         name: "Change font size",
@@ -129,8 +94,6 @@ var editor = new LoadEditor({
             {
                 opt: "select-fontsize",
                 title: "Font size",
-                urlparam: "fontsize",
-                chg_citation: "citation-url",
                 sizes: {
                     default: "default",
                     font_size_14: "14",
@@ -140,12 +103,9 @@ var editor = new LoadEditor({
                     font_size_32: "32"
                 },
                 paragraph: "p",
-                p_class: "yes-index-2",
-                css_class: "font-size-"
+                p_class: "yes-index-2"
             }
-        ],
-        active_class: "active",
-        html_class: "custom-select"
+        ]
     },
     is: {
         name: "enable/disable right window",
@@ -153,17 +113,6 @@ var editor = new LoadEditor({
             {
                 opt: "edition-switch1",
                 title: "Hide Right Window",
-                urlparam: "right-window",
-                chg_citation: "citation-url",
-                fade: "fade",
-                column_small: {
-                    "class": "col-md-6",
-                    "percent": "50%"
-                },
-                column_full: {
-                    "class": "col-md-12",
-                    "percent": "100%"
-                },
                 hide: {
                     hidden: true,
                     class_to_hide: "is-hide-container1",
@@ -174,7 +123,6 @@ var editor = new LoadEditor({
                 image_size: "1000px"
             }
         ],
-        active_class: "active",
         rendered_element: {
             a_class: "nav-link btn btn-round",
             svg: "ON / OFF"
